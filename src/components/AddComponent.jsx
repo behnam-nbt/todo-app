90f6ef07609492ecf7c63c88ada482d192fb3ca2
behnam nabati <behnamnabati0@gmail.com>
2024-07-29 20:03:19 +0200

    useRef added to AddComponent to fix the placeholder of date input

--- src/components/AddComponent.jsx
+++ src/components/AddComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 import styles from './AddComponent.module.css'
 
@@ -6,6 +6,27 @@ function AddComponent({ addTask }) {
 
   const [text, setText] = useState('');
   const [dueDate, setDueDate] = useState('');
+  const dateInputRef = useRef(null);
+
+  useEffect(() => {
+    const input = dateInputRef.current;
+    if (!input.value) {
+      input.placeholder = 'mm/dd/yy';
+    }
+    const handleFocus = () => input.placeholder = '';
+    const handleBlur = () => {
+      if (!input.value) {
+        input.placeholder = 'mm/dd/yy';
+      }
+    };
+    input.addEventListener('focus', handleFocus);
+    input.addEventListener('blur', handleBlur);
+
+    return () => {
+      input.removeEventListener('focus', handleFocus);
+      input.removeEventListener('blur', handleBlur);
+    };
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,8 +34,9 @@ function AddComponent({ addTask }) {
       addTask(text, dueDate);
       setText('');
       setDueDate('');
+      dateInputRef.current.placeholder = 'mm/dd/yy';
     }
-  }
+  };
 
   return (
     <>
@@ -24,7 +46,7 @@ function AddComponent({ addTask }) {
           <form onSubmit={handleSubmit}>
             <div className={styles.inputBox}>
               <input type="text" placeholder="Add Todo" value={text} onChange={(e) => setText(e.target.value)} />
-              <input type="date" placeholder="mm/dd/yy" value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
+              <input type="date"  ref={dateInputRef} placeholder="mm/dd/yy" value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
               <button className={styles.addtask} type="submit">Add</button>
             </div>
           </form>
