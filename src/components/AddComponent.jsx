72511ed6e548658fd844a31f4d85adee30865ace
behnam nabati <behnamnabati0@gmail.com>
2024-07-29 20:14:25 +0200

    AddComponent edited to fix placeHolder problem

--- src/components/AddComponent.jsx
+++ src/components/AddComponent.jsx
@@ -1,9 +1,7 @@
-import React, { useState, useRef, useEffect } from 'react'
-
-import styles from './AddComponent.module.css'
+import React, { useState, useRef, useEffect } from 'react';
+import styles from './AddComponent.module.css';
 
 function AddComponent({ addTask }) {
-
   const [text, setText] = useState('');
   const [dueDate, setDueDate] = useState('');
   const dateInputRef = useRef(null);
@@ -13,15 +11,21 @@ function AddComponent({ addTask }) {
     if (!input.value) {
       input.placeholder = 'mm/dd/yy';
     }
-    const handleFocus = () => input.placeholder = '';
+
+    const handleFocus = () => {
+      input.placeholder = '';
+    };
+
     const handleBlur = () => {
       if (!input.value) {
         input.placeholder = 'mm/dd/yy';
       }
     };
+
     input.addEventListener('focus', handleFocus);
     input.addEventListener('blur', handleBlur);
 
+    
     return () => {
       input.removeEventListener('focus', handleFocus);
       input.removeEventListener('blur', handleBlur);
@@ -39,21 +43,31 @@ function AddComponent({ addTask }) {
   };
 
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.addContainer}>
-          <h1>TODO APP</h1>
-          <form onSubmit={handleSubmit}>
-            <div className={styles.inputBox}>
-              <input type="text" placeholder="Add Todo" value={text} onChange={(e) => setText(e.target.value)} />
-              <input type="date"  ref={dateInputRef} placeholder="mm/dd/yy" value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
-              <button className={styles.addtask} type="submit">Add</button>
-            </div>
-          </form>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.addContainer}>
+        <h1>TODO APP</h1>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.inputBox}>
+            <input
+              type="text"
+              placeholder="Add Todo"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            <input
+              type="date"
+              ref={dateInputRef}
+              value={dueDate}
+              onChange={(e) => setDueDate(e.target.value)}
+            />
+            <button className={styles.addtask} type="submit">
+              Add
+            </button>
+          </div>
+        </form>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default AddComponent
+export default AddComponent;
